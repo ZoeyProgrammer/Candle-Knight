9d00fc498ea219af8711d95d827c1075f20e88c6
Zoey <prof.natasaki@gmail.com>
2021-06-18 10:37:35 +0200

    Menu Prep
    [+] Added basic Menu things

--- Assets/Scripts/UI/MenuManager.cs
+++ Assets/Scripts/UI/MenuManager.cs
@@ -7,6 +7,8 @@ public class MenuManager : MonoBehaviour
 {
 	[SerializeField] string gameStartScene = null;
 
+	[SerializeField] private GameObject mainMenu, settingsMenu, creditsMenu, controlsMenu = null;
+
 	public void StartGame()
 	{
 		SceneManager.LoadSceneAsync(gameStartScene);
@@ -14,7 +16,7 @@ public class MenuManager : MonoBehaviour
 
 	public void ResumeGame()
 	{
-		//Load newest Level from Game Manager?
+		//Load newest Level from Game Manager? Have list in gameManager with all the levels in array and a int which tells the newst level
 	}
 
 	public void LevelEditor()
@@ -22,18 +24,61 @@ public class MenuManager : MonoBehaviour
 		SceneManager.LoadSceneAsync("LevelEditor");
 	}
 
-	public void Settings()
+	public void OpenMenu(int menu)
 	{
-		//Open the settings menu
-	}
-
-	public void Credits()
-	{
-		//Open the Credits menu
+		switch (menu)
+		{
+			case 0: //Main Menu
+				mainMenu.SetActive(true);
+				settingsMenu.SetActive(false);
+				creditsMenu.SetActive(false);
+				controlsMenu.SetActive(false);
+				break;
+			case 1: //Settings Menu
+				mainMenu.SetActive(false);
+				settingsMenu.SetActive(true);
+				creditsMenu.SetActive(false);
+				controlsMenu.SetActive(false);
+				break;
+			case 2: //Credits Menu
+				mainMenu.SetActive(false);
+				settingsMenu.SetActive(false);
+				creditsMenu.SetActive(true);
+				controlsMenu.SetActive(false);
+				break;
+			case 3: //Controls Menu
+				mainMenu.SetActive(false);
+				settingsMenu.SetActive(false);
+				creditsMenu.SetActive(false);
+				controlsMenu.SetActive(true);
+				break;
+			default:
+				break;
+		}
 	}
 
 	public void QuitGame()
 	{
 		Application.Quit();
+	}
+
+	public void BrightnessSlider(int value)
+	{
+		//do Brightness
+	}
+
+	public void ContrastSlider(int value)
+	{
+		//Do Contrast
+	}
+
+	public void GammaSlider(int value)
+	{
+		//Do Gamma
+	}
+
+	public void VolumeSlider(int value)
+	{
+		//Do Volume
 	}
 }
