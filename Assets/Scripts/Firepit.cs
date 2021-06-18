d34043ef98fdf425c8a185c5ae14cf53eaf0fee1
Zoey <prof.natasaki@gmail.com>
2021-06-18 13:01:09 +0200

    Implemented Fire Pit
    [+] Fire Pit completly implemented

--- Assets/Scripts/Firepit.cs
+++ Assets/Scripts/Firepit.cs
@@ -1,13 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class Firepit : MonoBehaviour
 {
 	private bool isLit = false;
+	[SerializeField] private VisualEffect flameVFX = null;
 
 	private void OnTriggerEnter(Collider other)
 	{
-		
+		if (other.tag == "Player")
+		{
+			CharacterMovement player = other.gameObject.GetComponent<CharacterMovement>();
+			if (player != null && player.isLit && !isLit)
+			{
+				isLit = true;
+				flameVFX.Play();
+			}
+			else if (player != null && isLit && !player.isLit)
+			{
+				player.EnableFlame();
+			}
+		}
 	}
 }
